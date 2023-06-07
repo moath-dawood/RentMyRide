f95b801b720b39faa4d013f1c0b9374d6df79c35
Moath <m_darkknight@hotmail.com>
2023-06-07 17:06:15 +0300

    removed useless brackets

--- src/Components/RentMyRide/index.js
+++ src/Components/RentMyRide/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Typography } from '@mui/material'
 import { styled } from '@mui/material/styles';
 
-const Title = styled('div')(({}) => ({
+const Title = styled('div')(() => ({
   display: { xs: "block", sm: "block" },
   width: { xs: "100%", sm: "fit-content" },
   color: "#3563E9",
